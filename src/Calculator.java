9b4eb77dfe7853c1d38b094e0e87c081518e398a
Dane du Plessis <daneasc@gmail.com>
2017-09-06 12:55:57 +0200

    Some improvement to button layout - button widths still wrong

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,10 +5,10 @@ import java.awt.event.*;
 import javax.swing.*;
 
 class Calculator extends JFrame {
-	private JPanel controlPanel;
+	private JPanel content;
 	// data fields
-	private JTextField _dataTF = new JTextField(3);
-	// buttons
+	private JTextField _dataTF = new JTextField(10);
+	// buttons - consider using an array
 	JButton b1 = new JButton("1");
 	JButton b2 = new JButton("2");
 	JButton b3 = new JButton("3");
@@ -19,26 +19,37 @@ class Calculator extends JFrame {
 	JButton b8 = new JButton("8");
 	JButton b9 = new JButton("9");
 	JButton b0 = new JButton("0");
+	JButton bTimes = new JButton("*");
+	JButton bDivide = new JButton("/");
+	JButton bAdd = new JButton("+");
+	JButton bSubtract = new JButton("-");
+	JButton bPoint = new JButton(".");
+	JButton bEquals = new JButton("=");
 	
 	private static final int CONSTANT_VALUE = 5;
 	
+	private static final Insets insets = new Insets(2, 2, 2, 2);
+	
 	public Calculator() {
-		controlPanel = new JPanel();
-		controlPanel.setLayout(new FlowLayout());
 		// 0. Button layout objects
 		GridBagLayout gridLayout = new GridBagLayout();
 		GridBagConstraints gbc = new GridBagConstraints();
-		setFont(new Font("SansSerif", Font.BOLD, 14));
-		//setLayout(gridLayout);
+		//gbc.fill = GridBagConstraints.HORIZONTAL;
+		setFont(new Font("Monospaced", Font.PLAIN, 14));
 		
+		content = new JPanel();
+		content.setBackground(Color.white);
+		content.setLayout(gridLayout);
+		content.setMinimumSize(new Dimension(300,400));
+		content.setFont(new Font("Monospaced", Font.PLAIN, 14));
 		// 1. The panel to add them to.
-		JPanel p = new JPanel();
-		p.setBackground(Color.white);
-		p.setLayout(gridLayout);
+		content.setBackground(Color.white);
+		content.setLayout(gridLayout);
 		
 		// 2. The buttons to add
 		b1.setBackground(Color.ORANGE);
 		b2.setBackground(Color.ORANGE);
+		
 		b3.setBackground(Color.ORANGE);
 		b4.setBackground(Color.ORANGE);
 		b5.setBackground(Color.ORANGE);
@@ -48,85 +59,145 @@ class Calculator extends JFrame {
 		b9.setBackground(Color.ORANGE);
 		b0.setBackground(Color.ORANGE);
 		
+		b1.setSize(30, 35);
+		b2.setSize(30, 35);
+		b3.setSize(30, 35);
+		b4.setSize(30, 35);
+		b5.setSize(30, 35);
+		b6.setSize(30, 35);
+		b7.setSize(30, 35);
+		b8.setSize(30, 35);
+		b9.setSize(30, 35);
+		b0.setSize(30, 35);
 		
-		// 2. Add the buttons
-		
-		//gbc.fill = GridBagConstraints.REMAINDER;
-		gbc.weightx = 0;
-		//gbc.weighty = 0;
-		//gbc.anchor = GridBagConstraints.PAGE_START;
-		//gbc.ipadx = 30;
-		gbc.gridwidth = 3;
-		p.add(_dataTF, gbc);
-		
-		
-		//gbc.fill = GridBagConstraints.HORIZONTAL;
-		//gbc.weightx = 1;
-		gbc.ipady = 0;
-		gbc.ipadx = 0;
-		
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		p.add(b1, gbc);
-		
-		gbc.gridx = 1;
-		gbc.gridy = 1;
-		p.add(b2, gbc);
-		
-		gbc.gridx = 2;
-		gbc.gridy = 1;
-		p.add(b3, gbc);
-		//
-		
-		gbc.gridx = 0;
-		gbc.gridy = 2;
-		p.add(b4, gbc);
-		
-		gbc.gridx = 1;
-		gbc.gridy = 2;
-		p.add(b5, gbc);
+		JPanel row1 = new JPanel();
+		JPanel row2 = new JPanel();
+		JPanel row3 = new JPanel();
+		JPanel row4 = new JPanel();
+		JPanel row5 = new JPanel();
 
+		
+		// add display to row1
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		row1.setLayout(new GridBagLayout());
+		row1.add(_dataTF, gbc);
+		int gridy = 0;
+		addComponent(content,row1,0,gridy++,1,1,1D,1D,GridBagConstraints.CENTER,GridBagConstraints.BOTH);
+		
+		// add buttons to row2
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		row2.setLayout(new GridBagLayout());
+		row2.add(b7, gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		row2.add(b8, gbc);
+		
 		gbc.gridx = 2;
+		gbc.gridy = 0;
+		row2.add(b9, gbc);
+		
+		gbc.gridx = 3;
+		gbc.gridy = 0;
+		row2.add(bTimes, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		addComponent(content,row2,0,gridy++,1,1,1D,1D,GridBagConstraints.CENTER,GridBagConstraints.BOTH);
+		
+		// add buttons to row3
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		row3.setLayout(new GridBagLayout());
+		row3.add(b4, gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		row3.add(b5, gbc);
+		
+		gbc.gridx = 2;
+		gbc.gridy = 0;
+		row3.add(b6, gbc);
+		
+		gbc.gridx = 3;
+		gbc.gridy = 0;
+		row3.add(bDivide, gbc);
+		
+		gbc.gridx = 0;
 		gbc.gridy = 2;
-		p.add(b6, gbc);
-		// 
+		addComponent(content,row3,0,gridy++,1,1,1D,1D,GridBagConstraints.CENTER,GridBagConstraints.BOTH);
+		
+		// add buttons to row4
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		row4.setLayout(new GridBagLayout());
+		row4.add(b1, gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		row4.add(b2, gbc);
+		
+		gbc.gridx = 2;
+		gbc.gridy = 0;
+		row4.add(b3, gbc);
+		
+		gbc.gridx = 3;
+		gbc.gridy = 0;
+		row4.add(bAdd, gbc);
 		
 		gbc.gridx = 0;
 		gbc.gridy = 3;
-		p.add(b7, gbc);
+		addComponent(content,row4,0,gridy++,1,1,1D,1D,GridBagConstraints.CENTER,GridBagConstraints.BOTH);
+		
+		
+		// add buttons to row5
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		row5.setLayout(new GridBagLayout());
+		row5.add(b0, gbc);
 		
 		gbc.gridx = 1;
-		gbc.gridy = 3;
-		p.add(b8, gbc);
-
+		gbc.gridy = 0;
+		row5.add(bPoint, gbc);
+		
 		gbc.gridx = 2;
+		gbc.gridy = 0;
+		row5.add(bEquals, gbc);
+		
+		gbc.gridx = 3;
+		gbc.gridy = 0;
+		row5.add(bSubtract, gbc);
+		
+		gbc.gridx = 0;
 		gbc.gridy = 3;
-		p.add(b9, gbc);
-		//
-		
-		gbc.gridx = 1;
-		gbc.gridy = 4;
-		p.add(b0, gbc);
+		addComponent(content,row5,0,gridy++,1,1,1D,1D,GridBagConstraints.CENTER,GridBagConstraints.BOTH);
 		
 		
-		
-		controlPanel.add(p);
-		controlPanel.setMinimumSize(new Dimension(300,400));
-		
-		//setContentPane(p);
-		//pack();
-		//makeButton("", gridLayout, gbc);
 		
 		// 4. Set window's title and other attributes
-		setLayout(new GridLayout(1,1));
+		//setLayout(new GridLayout(1,1));
 		setTitle("Calculator");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setVisible(true);
-		add(controlPanel);
-		setMinimumSize(new Dimension(300,400));
+		add(content);
+		pack();
+		//setMinimumSize(new Dimension(300,400));
 		Image image = new ImageIcon("icons/calculatorImage2.png").getImage();
 		setIconImage(image);
+	}
+	
+	private void addComponent(Container container, Component component, 
+			int gridx, 		int gridy, 
+			int gridwidth, 	int gridheight, 
+			double weightx, double weighty, 
+			int anchor, 	int fill) {
+		GridBagConstraints gbc = new GridBagConstraints(gridx, gridy, gridwidth, gridheight, weightx, weighty, anchor, fill, insets, 0,0);
+		container.add(component, gbc);
 	}
 	
 	class CalculationBtnListener implements ActionListener {
